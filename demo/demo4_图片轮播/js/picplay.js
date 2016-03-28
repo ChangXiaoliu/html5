1312b4a803dc5aa99fc141ad161b50d94173530d
常晓柳 <1367115143@qq.com>
2016-03-28 18:08:45 +0800

    优化代码

--- demo/demo4_图片轮播/js/picplay.js
+++ demo/demo4_图片轮播/js/picplay.js
@@ -1,82 +1,56 @@
-window.onload = function() {
-      var oIco = document.getElementById("ico");
-      var aBtn = oIco.getElementsByTagName("a");
-      var oSlide = document.getElementById("slide");
-      var oUl = oSlide.getElementsByTagName("ul");
-      var aLi = oUl[0].getElementsByTagName("li");
-      var oBtnLeft = document.getElementById("btnLeft");
-      var oBtnRight = document.getElementById("btnRight");
-      
-      //每次打开页面，首先显示第一张
-      var baseWidth = aLi[0].offsetWidth;
-      oUl[0].style.width = baseWidth * aLi.length + "px";
-      //当前显示图片的位置索引
-      var iNow = 0;
+    window.onload = function() {
+        var wrap = document.getElementById('wrap'),
+            pic = document.getElementById('pic').getElementsByTagName("li"),
+            list = document.getElementById('list').getElementsByTagName('li'),
+            prev=document.getElementById("prev"),
+            next=document.getElementById("next"),
+            index = 0,
+            timer = null;
 
-      //遍历图片下方的图标数组，为每张图片添加点击事件，并修改iNow值
-      for(var i=0;i<aBtn.length;i++) {  
-        aBtn[i].index = i;
-        aBtn[i].onclick = function() {
-          move(this.index);
+        //定义并调用自动播放函数
+        timer = setInterval(autoPlay, 2000);
+
+        // 鼠标划过整个容器时停止自动播放
+        wrap.onmouseover = function() {
+            clearInterval(timer);
         }
-      }
 
-      //点击左/右侧按钮，并修改iNow值
-      oBtnLeft.onclick = function() {
-        iNow--;
-        move(iNow);
-      }
-      oBtnRight.onclick = function() {
-        iNow++;
-        move(iNow);
-      }
+        // 鼠标离开整个容器时继续播放至下一张
+        wrap.onmouseout = function() {
+                timer = setInterval(autoPlay, 2000);
+            }
+        // 遍历所有数字导航实现划过切换至对应的图片
+        for (var i = 0; i < list.length; i++) {
+            list[i].onmouseover = function() {
+                clearInterval(timer);
+                index = this.innerText - 1;
+                changePic(index);
+            };
+        };
 
-      // 原代码，不能从当前位置继续轮播
-      // var curIndex = 0;
-      // var timeInterval = 1000;
-      // setInterval(change,timeInterval);
-      // function change() {
-      //   if(curIndex == aBtn.length) {
-      //     curIndex =0;      
-      //   } else {
-      //     move(curIndex);
-      //     curIndex += 1;
-      //   }
-
-      // }
-
-      
-      //设置定时器，使图片自动轮播
-      // var curIndex = 0;
-      var timeInterval = 5000;
-      setInterval(function(){
-        change(iNow);
-      },timeInterval);
-
-      function change(curIndex) {
-
-        if(curIndex == aLi.length) {
-          curIndex =0;  
-        } 
-         move(curIndex);   
-         curIndex++;       
-         iNow=curIndex;
-      }
-
-      //移动索引到当前图片，改变当前图片的显示样式和父层的坐外边距 
-      function move(index) {
-        if(index>aLi.length-1) {
-          index = 0;
+        function autoPlay() {
+            if (++index >= pic.length) 
+              index = 0;
+            changePic(index);
         }
-         if(index<0) {
-          index = aLi.length-1;
+        next.onclick=function(){
+          clearInterval(timer);
+          autoPlay();
         }
-        for(var n=0;n<aBtn.length;n++) {
-          aBtn[n].className = ""; 
+        prev.onclick=function(){
+          clearInterval(timer);
+           if (--index < 0) 
+              index = 4;
+            changePic(index);
         }
-        //将当前图片索引赋给iNow
-        iNow = index;
-        aBtn[index].className = "active";
-        oUl[0].style.left = -index * baseWidth + "px";
-      }
-    }
+        // 定义图片切换函数
+        function changePic(curIndex) {
+            for (var i = 0; i < pic.length; ++i) {
+                pic[i].style.display = "none";
+                list[i].className = "";
+            }
+            pic[curIndex].style.display = "block";
+            list[curIndex].className = "on";
+        }
+
+    };
